4562bbdc3f6f7795da75753639c6a73ea50d74db
Surajchhetry <surajchhetry@gmail.com>
2018-06-12 17:17:56 +0545

    CORS and POST added

--- hello-app-engine/hello.go
+++ hello-app-engine/hello.go
@@ -6,6 +6,7 @@ import (
 
 	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"github.com/rs/cors"
 	"google.golang.org/appengine"
 )
 
@@ -16,10 +17,23 @@ type Customer struct {
 
 func main() {
 	router := httprouter.New()
+	c := cors.New(cors.Options{
+		AllowedHeaders:   []string{"Origin", "Accept", "Content-Type", "X-Requested-With"},
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "PUT", "POST", "OPTIONS", "HEADS"},
+		AllowCredentials: false,
+		MaxAge:           1200,
+	})
+	router.OPTIONS("/", options)
 	router.GET("/", Index)
+	router.POST("/", postTest)
 	router.GET("/hello/:name", Hello)
-	http.Handle("/", router)
+	http.Handle("/", c.Handler(router))
 	appengine.Main()
+}
+
+func options(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(200)
 }
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
@@ -29,4 +43,16 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	var std = Customer{Name: ps.ByName("name"), Age: 10}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(std)
+}
+
+func postTest(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var customer Customer
+	if r.Body == nil {
+		http.Error(w, " content is blank ", 400)
+	}
+	json.NewDecoder(r.Body).Decode(&customer)
+	w.Header().Set("Content-Type", "application/json")
+	customer.Name = " Hello ! " + customer.Name
+	json.NewEncoder(w).Encode(customer)
+
 }
